111b36fa217ea29375bd2d0fe7dbcd4063feb425
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2024-02-04 21:25:40 +0600

    project upload

---
+++ src/components/Tasks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task @toggle-reminder="$emit('toggle-reminder', task.id)" :task="task"
+          @delete-task="$emit('delete-task', task.id)"/>
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+  name: 'Tasks',
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Version 1.0.0</h1>
+  <router-link to="/">Go Back</router-link>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
